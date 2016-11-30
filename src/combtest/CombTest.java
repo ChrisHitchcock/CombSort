bbdcd5ed3780079441ed5d85e0911886511e3468
chhit5249 <chhit5249@7JGN6Y1.Student.UGDSB.ED>
2016-11-30 13:48:22 -0500

    V1.2 - Works and is formatted nicely

--- src/combtest/CombTest.java
+++ src/combtest/CombTest.java
@@ -16,37 +16,42 @@ public class CombTest {
      */
     public static void main(String[] args) {
         //Array Declaration
-        int[] nums = new int[]{151, 54, 89, 42, 12, 8, 7, 6, 5, 4, 2, 2, 1};
+        int[] nums = new int[]{421, 132, 32, 543, 123, 2, 54, 12, 51, 84, 21, 10, 6, 7};
         
         //Output
         System.out.println("Original Array:");
         for (int a = 0; a < nums.length; a++) {
             System.out.print(nums[a] + " ");
         }
-        System.out.println("\n--------------------------------\nSteps of Sorting: ");
+        System.out.println("\n---------------------------------------------------------------------"
+                + "\nSteps of Sorting:\t\t\t\tComparisons\tLoops");
         sort(nums);
-        System.out.println("--------------------------------\nSorted Array:");
+        System.out.println("---------------------------------------------------------------------"
+                + "\nSorted Array:");
         for (int a = 0; a < nums.length; a++) {
             System.out.print(nums[a] + " ");
         }
+        System.out.println("");
     }
 
     public static void sort(int[] input) {
         int gap = input.length;
         boolean swapped = true;
         int loops = 0;
+        int loops2 = 0;
         while (gap > 1 || swapped) {
+            loops2++;
             for (int a = 0; a < input.length; a++) {
                 System.out.print(input[a] + " ");
             }
-            System.out.println("\t" + loops);
+            System.out.println("\t" + loops+"\t\t"+loops2);
             if (gap > 1) {
                 gap = (int) (gap / 1.3);
             }
             swapped = false;
             for (int i = 0; i + gap < input.length; i++) {
 
-                if (input[i] > input[i + gap]) {
+                if (input[i] < input[i + gap]) {
                     int t = input[i];
                     input[i] = input[i + gap];
                     input[i + gap] = t;
