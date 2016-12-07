27ad5add138c4d5c2a802848cf11c9c98a8615cf
chhit5249 <chhit5249@7JGN6Y1.Student.UGDSB.ED>
2016-12-07 13:50:32 -0500

    Version 1.3 - Works great, good documentation and shows time it took to sort.

--- src/combtest/CombTest.java
+++ src/combtest/CombTest.java
@@ -4,19 +4,31 @@
  * and open the template in the editor.
  */
 package combtest;
-
+import java.util.*;
 /**
- *
+ * This program tests the comb sort.
  * @author chhit5249
  */
 public class CombTest {
     static int loops = 0;
+
     /**
+     * Everything happens here.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Array Declaration
-        int[] nums = new int[]{421, 132, 32, 543, 123, 2, 54, 12, 51, 84, 21, 10, 6, 7};
+        //Array/Variable Declaration
+        Random r = new Random();
+        int rand, total;
+        total = 12;
+        int[] nums = new int[total];
+        
+        //Generate random numbers for the array
+        for (int x=0;x<total;x++)
+        {
+            rand = r.nextInt(512);
+            nums[x]=rand;
+        }
         
         //Output
         System.out.println("Original Array:");
@@ -25,42 +37,62 @@ public class CombTest {
         }
         System.out.println("\n---------------------------------------------------------------------"
                 + "\nSteps of Sorting:\t\t\t\tComparisons\tLoops");
+        long t1 = System.currentTimeMillis();
         sort(nums);
+        long time = System.currentTimeMillis()-t1;
         System.out.println("---------------------------------------------------------------------"
                 + "\nSorted Array:");
         for (int a = 0; a < nums.length; a++) {
             System.out.print(nums[a] + " ");
         }
-        System.out.println("");
+        System.out.println("\n\nSorting took: "+time+"ms");
+       
     }
 
+    /**
+     * This method is how the sorting happens <p>
+     * pre: called
+     * post: array is sorted
+     * @param input Array
+     */
     public static void sort(int[] input) {
+        //Method variable declarations
         int gap = input.length;
         boolean swapped = true;
         int loops = 0;
         int loops2 = 0;
+        
+        //While loop until swapping is finished (i.e. gap <1 or swapped=false)
         while (gap > 1 || swapped) {
+            //Stuff for step by step output
             loops2++;
             for (int a = 0; a < input.length; a++) {
                 System.out.print(input[a] + " ");
             }
-            System.out.println("\t" + loops+"\t\t"+loops2);
+            
+            //Reduce gap if it is already > 1
             if (gap > 1) {
                 gap = (int) (gap / 1.3);
             }
+            
+            //Set swapped to false so array ends if no swap happens
             swapped = false;
+            
+            //For loop to go through array and swap via gap.
             for (int i = 0; i + gap < input.length; i++) {
-
+                //Int for number of comparisons
+                loops += 1;
+                
+                //If swap is necessary, swap
                 if (input[i] < input[i + gap]) {
                     int t = input[i];
                     input[i] = input[i + gap];
                     input[i + gap] = t;
                     swapped = true;
-                    loops += 1;
-                } else {
-                    loops += 1;
                 }
             }
+            //Format output of number of comparisons and how many loops
+            System.out.println("\t" + loops + "\t\t" + loops2);
         }
     }
 
