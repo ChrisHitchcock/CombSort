c443db14e39f9df83b7df8244ab746fd53e08e5a
chhit5249 <chhit5249@7JGN6Y1.Student.UGDSB.ED>
2016-12-13 13:55:50 -0500

    V1.6 - Now has a binary search method that works great and is commented/documented.

--- src/combtest/CombTest.java
+++ src/combtest/CombTest.java
@@ -20,14 +20,10 @@ public class CombTest {
         Random r = new Random();
         int rand, total;
         total = 12;
-        int[] nums = new int[total];
+        int[] nums = new int[]{4512,132,42,65,12,63,17,83,29};
         
         //Generate random numbers for the array
-        for (int x=0;x<total;x++)
-        {
-            rand = r.nextInt(512);
-            nums[x]=rand;
-        }
+        
         
         //Output
         System.out.println("Original Array:");
@@ -45,6 +41,20 @@ public class CombTest {
             System.out.print(nums[a] + " ");
         }
         System.out.println("\n\nSorting took: "+time+"ms");
+        
+        //Get the goal number
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Enter the desired number: ");
+        int goal = kb.nextInt();
+        
+        //Call binary search and output.
+        int ind = binSearch(nums, goal, 0, nums.length-1);
+        if (ind==-1)
+        {
+            System.out.println(goal+" was not found");
+        } else {
+            System.out.println(goal+" is at an index of "+ind+" in the array.");
+        }
     }
 
     /**
@@ -83,7 +93,7 @@ public class CombTest {
                 comps += 1;
                 
                 //If swap is necessary, swap
-                if (input[i] < input[i + gap]) {
+                if (input[i] > input[i + gap]) {
                     int t = input[i];
                     input[i] = input[i + gap];
                     input[i + gap] = t;
@@ -96,27 +106,32 @@ public class CombTest {
         }
     }
     
-    /*public static int binSearch(int arr[], int g) {
-        int mid = arr.length/2;
+    /**
+     * This method searches for the index of a given number in an array, using recursion.
+     * @param arr Array of numbers
+     * @param g Number to be found
+     * @param start Start of the search parameters
+     * @param end End of the search parameters
+     * @return -1 if not there, otherwise return index.
+     */
+    public static int binSearch(int arr[], int g, int start, int end) {
+        //Set the middle number
+        int mid = (start+end)/2;
+        
+        //For last loop; checks if number isn't in array
+        if (end<start)
+        {
+            return -1;
+        }       
+        
+        //If statements; when g finally is equal to mid, output. Otherwise, recursion
         if (g==arr[mid])
         {
-            return arr[mid];
+            return mid;
+        } else if (g<arr[mid]) {
+            return binSearch(arr,g,0,mid-1);
+        } else {
+            return binSearch(arr,g,mid+1,end);
         }
-        else if (g<arr[mid])
-        {
-            int[] upperHalf = new int[arr.length/2];
-            for (int i = arr.length/2;i<arr.length;i++)
-            {
-                for (int x = 0; x<arr.length/2;i++)
-                upperHalf[x]=arr[i];
-            }
-            binSearch(upperHalf,g);
-        }
-        else if (g>arr[mid])
-        {
-            
-        }
-        return arr[mid];
-    }*/
-
+    }
 }
